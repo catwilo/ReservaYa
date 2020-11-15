c9fe0b8cc1015d7f018420e499062d09ab322a32
catwilo <31023673+catwilo@users.noreply.github.com>
2020-11-15 10:21:49 -0500

    AVL en proceso

--- src/main/java/Structure/AVL.java
+++ src/main/java/Structure/AVL.java
@@ -10,16 +10,107 @@ package Structure;
  * @author W1
  * @param <T>
  */
-public class AVL <T extends Comparable> extends BTree<T> {
-    AVLNode  root;
+public class AVL<T extends Comparable> {
+
+    AVLNode root;
+
     public AVL() {
-        super();
+        this.root = null;
     }
 
-    @Override
-    public TrieNode insert(T data) {
+    public void makeEmpty() {
+        this.root = null;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    private int height(TrieNode temp) {
+        if (temp == null) {
+            return -1;
+        } else {
+            return 1 + Math.max(height(temp.getLeft()), height(temp.getRight()));
+        }
+    }
+
+    public AVLNode insert(T data) {
+        //devuelve el nodo donde inserto el valor data
+        AVLNode aux = insert(data, this.root);
+        return aux;
+    }
+
+    private AVLNode insert(T data, AVLNode temp) {
+        if (temp == null) {
+            temp = new AVLNode(data);
+            return temp;
+        }
+        if (data.compareTo(temp.getData()) > 0) {
+            temp.setRight(insert(data, temp.getRight()));
+        } else if (data.compareTo(temp.getData()) < 0) {
+            temp.setLeft(insert(data, temp.getLeft()));
+        }
+        /*else { // para valores duplicados
+            
+        }*/
+        return temp;
+    }
+
+    public AVLNode findMin(AVLNode temp) {
+        if (temp == null) {
+            return null;
+        }
+        if (temp.getLeft() == null) {
+            return temp;
+        }
+
+        return findMin(temp.getLeft());
+    }
+    public AVLNode remove(T data) {
+        return remove(data, root);
+    }
+    public AVLNode contains(T data) {
+        return contains(data, this.root);
+    }
+     private AVLNode remove(T data, AVLNode temp) {
+        if (temp == null) {
+            return temp;
+        }
+        int comparacion = data.compareTo(temp.getData());
+        if (comparacion < 0) {
+            temp.setLeft(remove(data, temp.getLeft()));
+        } else if (comparacion > 0) {
+            temp.setRight(remove(data, temp.getRight()));
+        } else if (temp.getLeft() != null && temp.getRight() != null) {
+            //cuanto compare es 0 entra a este condicional 
+            //pues temp tiene el dato a borrar
+            //le doy el valor de su sucesor a temp para borrar el valor data
+            temp.setData(findMin(temp.getRight()).getData());
+            //y elimino la copia del sucesor que pertenecia al valor borrado
+            temp.setRight(remove((T) temp.getData(), temp.getRight()));
+        } else {
+            temp = (temp.getLeft() != null) ? temp.getLeft() : temp.getRight();
+        }
+        return temp;
+    }
+
+    private AVLNode contains(T data, AVLNode temp) {
+        //devuelve el nodo con el dato o el nodo que seria el padre
+        if (temp == null) {
+            return temp;
+        }
+        int comparacion = data.compareTo(temp.getData());
+        if (comparacion > 0) {
+            return contains(data, temp.getRight());
+        }
+        if (comparacion < 0) {
+            return contains(data, temp.getLeft());
+
+        }
+        /*else { // para valores duplicados
+            
+        }*/
+
         return null;
     }
-    
-
 }
--- src/main/java/Structure/AVLNode.java
+++ src/main/java/Structure/AVLNode.java
@@ -10,10 +10,47 @@ package Structure;
  * @author W1
  * @param <T>
  */
-public class AVLNode <T extends Comparable> extends TrieNode {
-    int height;
-    
-    public AVLNode (T data){
-        super(data);
+public class AVLNode<T extends Comparable> {
+
+    private int height;
+    T data;
+    private AVLNode right;
+    private AVLNode left;
+
+    public AVLNode(T data) {
+        this.data = data;
     }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public AVLNode getRight() {
+        return right;
+    }
+
+    public void setRight(AVLNode right) {
+        this.right = right;
+    }
+
+    public AVLNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(AVLNode left) {
+        this.left = left;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
 }
